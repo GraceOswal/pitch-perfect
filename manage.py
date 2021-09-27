dcea2b098dd91be7bc2998b289c8b2f09666a277
graceoswal <graceoswal88@gmail.com>
2021-09-27 05:53:30 -0400

    --- Auto-Git Commit ---

--- manage.py
+++ manage.py
@@ -1,10 +1,23 @@
-from app import create_app,db
 
-#imports the db instance
+from app import create_app,db
+from flask_script import Manager, Server
 from app.models import User
+import app
+
+app = create_app()
+
+manager = Manager(app)
+manager.add_command('server', Server(use_debugger=True))
 
 @manager.shell
 def make_shell_context():
-    return dict(app = app, db = db, User = User )
+    return {'app': app, 'db' : db, 'User' : User }
+
+@manager.command
+def test():
+    import unittest
+    tests = unittest.TestLoader().discover('tests')
+    unittest.TextTestRunner().run(tests)
+
 if __name__ == '__main__':
     manager.run()
