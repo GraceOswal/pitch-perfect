1885e33298f5bca40f13a76c045f7605eb93ef65
graceoswal <graceoswal88@gmail.com>
2021-09-28 10:12:16 -0400

    --- Auto-Git Commit ---

--- app/__init__.py
+++ app/__init__.py
@@ -4,6 +4,9 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_bootstrap import Bootstrap
 from flask_mail import Mail
 
+from app import views
+from app import error
+
 from config import Config
 
 app = Flask(__name__)
