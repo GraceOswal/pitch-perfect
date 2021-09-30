6e22d1d56e9c099bcd35623ea94895eb0e0dd3cc
graceoswal <graceoswal88@gmail.com>
2021-09-30 02:55:45 -0400

    add configurations

--- config.py
+++ config.py
@@ -3,4 +3,7 @@ import os
 class Config:
     debug = True
     SECRET_KEY = os.environ.get('DATABASE_URL')
-    SQLALCHEMY_TDATABASE_URI = 'postgresql+psycopg2://graceinyua:gee@localhost/pitch-perfect'
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://graceinyua:gee@localhost/pitch-perfect'
+    SQLALCHEMY_TRACK_MODIFICATIONS = True
+
+    
